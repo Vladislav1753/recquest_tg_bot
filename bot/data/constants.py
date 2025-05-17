4056456cef695e3f60a9bdd6ba4e5c08e40764f2
Vladyslav <vladbaklanov04@gmail.com>
2025-05-17 12:57:16 +0700

    Refactor: moved API keys to .env and added example file

--- bot/data/constants.py
+++ bot/data/constants.py
@@ -1,2 +1,7 @@
-TELEGRAM_TOKEN = ""
-GEMINI_API_KEY = ""
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
